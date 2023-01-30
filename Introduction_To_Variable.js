d3f1a7e88b725f22db588c6448db45ab2358cd99
Sk Mantajur Islam <120780547+MANTAJUR@users.noreply.github.com>
2023-01-31 04:19:27 +0530

    Create Introduction_To_Variable.js
    pull it

---
+++ Introduction_To_Variable.js
@@ -0,0 +1,13 @@
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+let name="Sk Mantajur Islam";
+
+console.log(name);
+let fn="Sk Abdul Sattar";
+
+console.log(fn);
+let mn="Bela Bibi";
+
+console.log(mn);
